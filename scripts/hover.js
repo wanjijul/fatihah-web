5ca59725e1a41d15d5d6f3dc7d1fb34786b77ffe
Aiman Md Uslim <amduslim@purdue.edu>
2017-01-03 07:47:59 +0800

    show hide sorta working

--- scripts/hover.js
+++ scripts/hover.js
@@ -2,39 +2,20 @@
 
 $(document).ready(function(){
     $('map').imageMapResize();
-    $('#fatihah').maphilight();  
+//    $('#fatihah').maphilight();  
     $('#fatihah').maphilight().parent().addClass('center-map');
     
     var present = false;
     
     $('#ayat2-area').click(function(e){
-        if(e){
-            console.log(e);
-        } else {
-            if(!present){
-                $("#ayat2-text").show();
-                present = true; 
-                
-            } else {
-                $('.contents').hide();                
-                $("#ayat2-text").show();
-            }
-            
-        }
+        e.preventDefault();
+        console.log('2 clicked!');
+        $('#ayat2-text').show(3000);
     })
     
     $('#ayat3-2-area').click(function(e){
-      if(e){ console.log(e); }
-        else{
-            if(!present){
-                $("#ayat3-2-text").show();
-                present = true; 
-                
-            } else {
-                $('.contents').hide();                
-                $("#ayat3-2-text").show();
-            }
-        }
+        e.preventDefault();
+        console.log('3-2 clicked!');
     })
     
     
