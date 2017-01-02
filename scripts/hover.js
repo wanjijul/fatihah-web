5f781be9e16f5fda2839521189c7ddd620c147d3
Aiman Md Uslim <amduslim@purdue.edu>
2017-01-03 07:20:22 +0800

    trying to implement clicking

--- scripts/hover.js
+++ scripts/hover.js
@@ -5,5 +5,37 @@ $(document).ready(function(){
     $('#fatihah').maphilight();  
     $('#fatihah').maphilight().parent().addClass('center-map');
     
+    var present = false;
+    
+    $('#ayat2-area').click(function(e){
+        if(e){
+            console.log(e);
+        } else {
+            if(!present){
+                $("#ayat2-text").show();
+                present = true; 
+                
+            } else {
+                $('.contents').hide();                
+                $("#ayat2-text").show();
+            }
+            
+        }
+    })
+    
+    $('#ayat3-2-area').click(function(e){
+      if(e){ console.log(e); }
+        else{
+            if(!present){
+                $("#ayat3-2-text").show();
+                present = true; 
+                
+            } else {
+                $('.contents').hide();                
+                $("#ayat3-2-text").show();
+            }
+        }
+    })
+    
     
 })
