9f9c3ee131f144a241a0e9ce536cb44c5b09344b
Aiman Md Uslim <aiman.md.uslim@intel.com>
2017-01-05 17:33:34 +0800

    need to work more

--- scripts/hover.js
+++ scripts/hover.js
@@ -1,10 +1,29 @@
 
 
 $(document).ready(function(){
-    $('map').imageMapResize();
+    // $('map').imageMapResize();
 //    $('#fatihah').maphilight();  
-    $('#fatihah').maphilight().parent().addClass('center-map');
-    
+    // $('#fatihah').maphilight().parent().addClass('center-map');
+ 
+    var image = $('#fatihah');
+
+    $('#fatihah').mapster({
+    	mapKey: 'name',
+    	singleSelect: true,
+        fillOpacity: 0.6,
+        fillColor: 'FF0000',
+    	onMouseover: function(e, opt, key, selected){
+    		console.log(e);
+   //  		var ayatnumber = event;
+			// ayatnumber = ayatnumber.split('-')[0];
+			// console.log(ayatnumber);
+   //  		$('area[name=' + ayatnumber + '-area]').mouseenter();
+	    }
+    })
+
+
+
+
 	$('area').click(function(e){
 		e.preventDefault()
 		var ayatnumber = $(this).attr('class');
@@ -20,21 +39,46 @@ $(document).ready(function(){
 	})
 	
 	var hovered = false;
-	
-	$('area').mouseover(function(e){
-		e.preventDefault();
-		var ayatnumber = $(this).attr('class');
-		ayatnumber = ayatnumber.split('-')[0];
-		if(hovered === false){
-			hovered = true;
-			$('.'+ayatnumber+'-area').mouseover();
-		}
+	var current_hovered_object_id = "";
+
+	// $('area').mouseover(function(e){
+	// 	e.preventDefault();
+
+	// 	// $(this).mapster('highlight');
+	// 	current_hovered_object_id = $(this).attr('id');
+
+
+	// 	var ayatnumber = $(this).attr('class');
+	// 	console.log(current_hovered_object_id);
+
+	// 	$('#'+current_hovered_object_id).mapster({
+	//         // mapKey: 'name',
+	//         singleSelect: true,
+	//         fillOpacity: 0.6,
+	//         fillColor: 'FF0000',
+ //        	onMouseover: function(){
+ //        		$(this).addClass('hover');
+ //    	    }
+
+	//     });
+
+	// 	// ayatnumber = ayatnumber.split('-')[0];
+	// 	// if(hovered === false){
+	// 	// 	hovered = true;
+	// 	// 	$('.'+ayatnumber+'-area').mapster('highlight');
+	// 	// 	$('.'+ayatnumber+'-area').mouseover();			
+	// 	// }
 		
-	})
+	// })
 	
-	$('area').mouseleave(function(e){
-		hovered = false;
-	})
+	// $('area').mouseleave(function(e){
+	// 	e.preventDefault();
+	// 	console.log('left');
+	// 	// $('#'+current_hovered_object_id).mapster('highlight', false);
+	// 	// $('#fatihah').mapster('highlight',false); 
+
+	// 	hovered = false;
+	// })
 	
 	
 	
